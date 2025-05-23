9944a0aff27cef720f3214279e66dcccb2026db3
igndu05 <idurmes337@g.educaand.es>
2025-05-23 19:27:43 +0200

    Inserción de código en clases Artista y Usuario

--- src/main/java/entidades/Artista.java
+++ src/main/java/entidades/Artista.java
@@ -1,5 +1,144 @@
 package entidades;
 
-public class Artista {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "artistas")
+
+@NamedQueries({
+    @NamedQuery(name = "Artista.findAll", query = "SELECT a FROM Artistas a"),
+    @NamedQuery(name = "Artista.findById", query = "SELECT a FROM Artistas a WHERE a.id = :id"),
+    @NamedQuery(name = "Artista.findByNombre", query = "SELECT a FROM Artistas a WHERE a.nombre = :nombre"),
+    @NamedQuery(name = "Artista.findByNif", query = "SELECT a FROM Artistas a WHERE a.nif = :nif")})
+public class Artista implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "codArtista")
+    private Integer codArtista;
+    @Column(name = "nomArtista")
+    private String nomArtista;
+    @Basic(optional = false)
+    @Column(name = "fechaNacimientoArtista")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDate fechaNacimientoArtista;
+    
+
+    @OneToMany(mappedBy = "artistas", cascade = CascadeType.PERSIST)
+    private Collection<Disco> discoCollection;
+
+    public Artista() {
+    }
+
+    public Artista(Integer codArtista) {
+        this.codArtista = codArtista;
+    }
+
+    public Artista(Integer codArtista, String nomArtista, LocalDate fechaNacimientoArtista, Collection<Disco> discoCollection) {
+        this.codArtista = codArtista;
+        this.nomArtista = nomArtista;
+        this.fechaNacimientoArtista = fechaNacimientoArtista;
+        this.discoCollection = new ArrayList<>();
+    }
+
+    public Integer getCodArtista() {
+        return codArtista;
+    }
+
+    public void setCodArtista(Integer codArtista) {
+        this.codArtista = codArtista;
+    }
+
+    public String getNomArtista() {
+        return nomArtista;
+    }
+
+    public void setNomArtista(String nomArtista) {
+        this.nomArtista = nomArtista;
+    }
+
+    public LocalDate getFechaNacimientoArtista() {
+        return fechaNacimientoArtista;
+    }
+
+    public void setFechaNacimientoArtista(LocalDate fechaNacimientoArtista) {
+        this.fechaNacimientoArtista = fechaNacimientoArtista;
+    }
+
+    public Collection<Disco> getDiscoCollection() {
+        return discoCollection;
+    }
+
+    public void setDiscoCollection(Collection<Disco> discoCollection) {
+        this.discoCollection = discoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.codArtista);
+        hash = 59 * hash + Objects.hashCode(this.nomArtista);
+        hash = 59 * hash + Objects.hashCode(this.fechaNacimientoArtista);
+        hash = 59 * hash + Objects.hashCode(this.discoCollection);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Artista other = (Artista) obj;
+        if (!Objects.equals(this.nomArtista, other.nomArtista)) {
+            return false;
+        }
+        if (!Objects.equals(this.codArtista, other.codArtista)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaNacimientoArtista, other.fechaNacimientoArtista)) {
+            return false;
+        }
+        return Objects.equals(this.discoCollection, other.discoCollection);
+    }
+    
+    // FALTA SET ID CLIENTE DE LA CLASE DISCO
+    
+    @Override
+    public String toString() {
+        String tmp = "";
+        // CORREGIR TO STRING, FALTA CREAR LA CLASE DISCO
+        // for (Venta venta : ventaCollection) {
+        //     tmp+=venta+"\n";
+        // }
+        return "Artista{" + "codArtista=" + codArtista + ", nomArtista=" + nomArtista + ", fechaNacimientoArtista=" + fechaNacimientoArtista + ", discoCollection=" + discoCollection + '}';
+    }
+    
+    
     
 }
--- src/main/java/entidades/Usuario.java
+++ src/main/java/entidades/Usuario.java
@@ -1,5 +1,175 @@
 package entidades;
 
-public class Usuario {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuarios u"),
+    @NamedQuery(name = "Usuario.findById", query = "SELECT u FROM Usuarios u WHERE u.id = :id"),
+    @NamedQuery(name = "Usuario.findByNombre", query = "SELECT u FROM Usuarios u WHERE u.nombre = :nombre"),
+    @NamedQuery(name = "Usuario.findByNif", query = "SELECT u FROM Usuarios u WHERE u.nif = :nif")})
+public class Usuario implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "codUsuario")
+    private Integer codUsuario;
+    @Column(name = "nomUsuario")
+    private String nombreUsuario;
+    @Basic(optional = false)
+    @Column(name = "dniUsuario")
+    private String dniUsuario;
+    @Basic(optional = false)
+    @Column(name = "localidadUsuario")
+    private String localidadUsuario;
+    @Basic(optional = false)
+    @Column(name = "telefUsuario")
+    private String telefUsuario;
+    @Basic(optional = false)
+
+    @OneToMany(mappedBy = "usuarios", cascade = CascadeType.PERSIST)
+    private Collection<Disco> discoCollection; 
+
+    // Constructores
+    public Usuario() {
+    }
+
+    public Usuario(Integer codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    public Usuario(Integer codUsuario, String dniUsuario, String localidadUsuario, String telefUsuario) {
+        this.codUsuario = codUsuario;
+        this.dniUsuario = dniUsuario;
+        this.localidadUsuario = localidadUsuario;
+        this.telefUsuario = telefUsuario;
+        this.discoCollection = new ArrayList<>();
+    }
+
+    // Getters y Setters
+    public Integer getCodUsuario() {
+        return codUsuario;
+    }
+
+    public void setCodUsuario(Integer codUsuario) {
+        this.codUsuario = codUsuario;
+    }
+
+    public String getNombre() {
+        return nombreUsuario;
+    }
+    public void setNombre(String nombre) {
+        this.nombreUsuario = nombre;
+    }
+    public String getDniUsuario() {
+        return dniUsuario;
+    }
+    public void setDniUsuario(String dniUsuario) {
+        this.dniUsuario = dniUsuario;
+    }
+    public String getLocalidadUsuario() {
+        return localidadUsuario;
+    }
+    public void setLocalidadUsuario(String localidadUsuario) {
+        this.localidadUsuario = localidadUsuario;
+    }
+    public String getTelefUsuario() {
+        return telefUsuario;
+    }
+    public void setTelefUsuario(String telefUsuario) {
+        this.telefUsuario = telefUsuario;
+    }
+    public Collection<Disco> getDiscoCollection() {
+        return discoCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codUsuario == null) ? 0 : codUsuario.hashCode());
+        result = prime * result + ((nombreUsuario == null) ? 0 : nombreUsuario.hashCode());
+        result = prime * result + ((dniUsuario == null) ? 0 : dniUsuario.hashCode());
+        result = prime * result + ((localidadUsuario == null) ? 0 : localidadUsuario.hashCode());
+        result = prime * result + ((telefUsuario == null) ? 0 : telefUsuario.hashCode());
+        result = prime * result + ((discoCollection == null) ? 0 : discoCollection.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (codUsuario == null) {
+            if (other.codUsuario != null)
+                return false;
+        } else if (!codUsuario.equals(other.codUsuario))
+            return false;
+        if (nombreUsuario == null) {
+            if (other.nombreUsuario != null)
+                return false;
+        } else if (!nombreUsuario.equals(other.nombreUsuario))
+            return false;
+        if (dniUsuario == null) {
+            if (other.dniUsuario != null)
+                return false;
+        } else if (!dniUsuario.equals(other.dniUsuario))
+            return false;
+        if (localidadUsuario == null) {
+            if (other.localidadUsuario != null)
+                return false;
+        } else if (!localidadUsuario.equals(other.localidadUsuario))
+            return false;
+        if (telefUsuario == null) {
+            if (other.telefUsuario != null)
+                return false;
+        } else if (!telefUsuario.equals(other.telefUsuario))
+            return false;
+        if (discoCollection == null) {
+            if (other.discoCollection != null)
+                return false;
+        } else if (!discoCollection.equals(other.discoCollection))
+            return false;
+        return true;
+    }
+
+    
+    
+    // FALTA SET ID CLIENTE DE LA CLASE DISCO
+
+    
+    @Override
+    public String toString() {
+        String tmp = "";
+        // CORREGIR TO STRING, FALTA CREAR LA CLASE DISCO
+        // for (Venta venta : ventaCollection) {
+        //     tmp+=venta+"\n";
+        // }
+        return "Usuario{" + "codUsuario=" + codUsuario + ", nombreUsuario=" + nombreUsuario + ", dniUsuario=" + dniUsuario + ", localidadUsuario=" + localidadUsuario + ", telefUsuario=" + telefUsuario + ", discoCollection=" + discoCollection + '}';
+    }
+
     
 }
